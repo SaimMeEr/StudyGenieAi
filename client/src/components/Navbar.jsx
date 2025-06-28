4e4a28446a3df28baa6a029ecb64e52fa5260c39
narsi-jangid <narsijangid01@gmail.com>
2025-06-28 18:26:02 +0530

    url changed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <div className='flex items-center justify-between py-3'>
             <Link to={"/"} className='text-3xl font-bold font-mono'>
-                Quizzy
+              StudyGenie
             </Link>
             <div className='flex gap-5'>
                 <NavLink to={"/"} className={({ isActive }) => isActive === true ? "text-green-600" : "text-white"}>
